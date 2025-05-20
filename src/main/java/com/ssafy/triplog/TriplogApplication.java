df3541b85eb4900bdea04c56d1012eed396f9134
pong <parkdanil@naver.com>
2025-05-20 14:21:45 +0900

    feat: MyBatis setting

--- src/main/java/com/ssafy/triplog/TriplogApplication.java
+++ src/main/java/com/ssafy/triplog/TriplogApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ssafy.triplog.user.mapper")
 public class TriplogApplication {
     public static void main(String[] args) {
         SpringApplication.run(TriplogApplication.class, args);
